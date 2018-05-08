36b0a027337af412cd44c5dced37099c3e01c341
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-05-08 12:45:15 +0100

    Add more info to the hash in key: value pairs

--- directory.rb
+++ directory.rb
@@ -162,7 +162,6 @@ students = input_students
 print_header
 print_students_less_than_twelve_chars(students)
 print_footer(students)
-=end
 
 # Rewrite the each() method for printing students to use while control flow
 def input_students
@@ -220,6 +219,64 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 students = input_students
+# nothing happens until we call the methods
+print_header
+print(students)
+print_footer(students)
+=end
+
+# Add some more info to the hash
+students = [
+  {name: "Dr. Hannibal Lecter", cohort: :november, hobbies: "eating", country_of_birth: "Denmark", height: 2.00},
+  {name: "Darth Vader", cohort: :november, hobbies: "breathing", country_of_birth: "Tatooine", height: 2.10},
+  {name: "Nurse Ratched", cohort: :november, hobbies: "reading", country_of_birth: "USA", height: 1.67},
+  {name: "Michael Corleone", cohort: :november, hobbies: "jogging", country_of_birth: "USA", height: 1.89},
+  {name: "Alex DeLarge", cohort: :november, hobbies: "theatre going", country_of_birth: "USA", height: 2.01},
+  {name: "The Wicked Witch of the West", cohort: :november, hobbies: "flying", country_of_birth: "USA", height: 1.68},
+  {name: "Terminator", cohort: :november, hobbies: "terminating", country_of_birth: "Austria", height: 2.15},
+  {name: "Freddy Krueger", cohort: :november, hobbies: "chopping", country_of_birth: "USA", height: 1.87},
+  {name: "The Joker", cohort: :november, hobbies: "poetry", country_of_birth: "USA", height: 1.91},
+  {name: "Joffrey Baratheon", cohort: :november, hobbies: "swimming", country_of_birth: "USA", height: 1.89},
+  {name: "Norman Bates", cohort: :november, hobbies: "cooking", country_of_birth: "Australia", height: 1.92}
+]
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+# Print a numbered list of students starting with their position in the hash using a while loop
+def print(students)
+  students_index = 0
+  while students.length > students_index do
+    puts "#{students_index + 1}. #{students[students_index][:name]} (#{students[students_index][:cohort]} cohort)"
+    puts "Height: #{students[students_index][:height]} metres"
+    puts "Who enjoys #{students[students_index][:hobbies]} and was born in #{students[students_index][:country_of_birth]}"
+    students_index += 1
+  end
+end
+
+def print_students_beginning_with(students)
+  puts "What letter who you like: "
+  letter = gets.chomp
+  students.each do |student|
+    if student[:name].chars[0] == letter.upcase
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    end
+  end
+end
+
+ def print_students_less_than_twelve_chars(students)
+   students.each do |student|
+     if student[:name].length < 12
+     puts "#{student[:name]} (#{student[:cohort]} cohort)"
+     end
+   end
+end
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
+end
 # nothing happens until we call the methods
 print_header
 print(students)
