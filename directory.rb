edfb7be445ab7be334b2311f50fad55e864bd8f8
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-05-08 12:52:32 +0100

    Centre the text

--- directory.rb
+++ directory.rb
@@ -223,7 +223,6 @@ students = input_students
 print_header
 print(students)
 print_footer(students)
-=end
 
 # Add some more info to the hash
 students = [
@@ -277,6 +276,67 @@ end
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
+# nothing happens until we call the methods
+print_header
+print(students)
+print_footer(students)
+=end
+
+# Display the info using center()
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+
+def print_header
+  puts "The students of Villains Academy".center(50)
+  puts "-------------".center(50)
+end
+
+def print(students)
+  students_index = 0
+  while students.length > students_index do
+    puts "#{students_index + 1}. #{students[students_index][:name]} (#{students[students_index][:cohort]} cohort)".center(50)
+    students_index += 1
+  end
+end
+
+def print_students_beginning_with(students)
+  puts "What letter who you like: "
+  letter = gets.chomp
+  students.each do |student|
+    if student[:name].chars[0] == letter.upcase
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    end
+  end
+end
+
+ def print_students_less_than_twelve_chars(students)
+   students.each do |student|
+     if student[:name].length < 12
+     puts "#{student[:name]} (#{student[:cohort]} cohort)"
+     end
+   end
+end
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students".center(50)
+end
+students = input_students
 # nothing happens until we call the methods
 print_header
 print(students)
