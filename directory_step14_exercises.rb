d876402d679b6316c65fd2bcae4c4f3b1763ccdf
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-05-10 09:58:50 +0100

    Load students.csv as default if it exists

---
+++ directory_step14_exercises.rb
@@ -0,0 +1,124 @@
+@students = [] #an empty array accessible to all methods
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
+  puts "9. Exit" # 9 because we'll add more items
+end
+
+def interactive_menu
+  # declare the students variable outside the loop as if inside it wouldn't be available for all iterations of the loop.
+  # students = []
+  # 4. Be able to keep repeating from step 1
+  loop do
+  # 1. print the menu and ask the user what to do
+    print_menu
+    # 2. read the input and save it into a variable
+    # 3. do what the user has asked
+    process(STDIN.gets.chomp)
+  end
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "3"
+    save_students
+  when "4"
+    load_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, try again"
+  end
+end
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice times"
+  # get the first name
+  name = STDIN.gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    add_students(name, cohort = "november")
+      if @students.count < 2
+        puts "Now we have #{@students.count} student"
+      else puts "Now we have #{@students.count} students"
+      end
+      # get another name from the user
+    name = STDIN.gets.chomp
+  end
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print_students_list
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
+end
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+def load_students(filename = "students.csv")
+  # open the file for reading
+  file = File.open(filename, "r")
+  # iterate over the contents of the file
+  file.readlines.each do |line|
+    # split every line at the comma, which gives an array with two elements, assign to the name and cohort variables.
+    # this is a parallel assignment (assigning two variables at the same time)
+    name, cohort = line.chomp.split(",")
+    # put a new hash into the @students array, with the cohort as a symbol for consistency
+    add_students(name, cohort)
+  end
+  # close the file
+  file.close
+end
+
+def add_students(name, cohort)
+  @students << {name: name, cohort: cohort.to_sym}
+end
+
+def try_load_students
+  filename = "students.csv" # first arguement from the command line
+   # get out of this method if it isn't given
+  if File.exists? (filename) #if it exists
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else # if it doesn't exist
+    puts "Sorry #{filename} doesn't exist."
+    return # return so that you can start the programme and mae the CSV if one doesn't already exist
+  end
+end
+
+try_load_students
+interactive_menu
